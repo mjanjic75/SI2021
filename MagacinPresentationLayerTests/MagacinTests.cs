b78dad63c40c4aefa58b0debae78295b5cc46964
mjanjic75 <mjanjic75@gmail.com>
2021-12-07 12:10:07 +0100

    Dodat Unit test GetAllItems metode na prezentacionom (Dekstop) sloju

---
+++ MagacinPresentationLayerTests/MagacinTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Shared.Interfaces;
+using Shared.Models;
+
+namespace MagacinPresentationLayerTests
+{
+    [TestClass]
+    public class MagacinTests
+    {
+        [TestMethod]
+        public void IsDataRefreshed()
+        {
+            // Arrange
+
+            List<Item> listOfItems = new List<Item>();
+            listOfItems.Add(new Item
+            {
+                Name = "Red Bull 0.5L",
+                Price = 159.90m,
+                Discount = 5
+            });
+            listOfItems.Add(new Item
+            {
+                Name = "Žele kocka 200g",
+                Price = 129.90m,
+                Discount = 0
+            });
+            listOfItems.Add(new Item
+            {
+                Name = "Štark bananica",
+                Price = 14.90m,
+                Discount = 0
+            });
+
+            var mockItemBusiness = new Mock<IItemBusiness>();
+            mockItemBusiness.Setup(s => s.GetAllItems()).Returns(listOfItems);
+
+            var magacinForm = new Magacin.Magacin(mockItemBusiness.Object);
+            // ako namespace i klasa imaju isti naziv, onda mora ovako kao u kodu iznad
+
+
+            // Act
+
+            PrivateObject obj = new PrivateObject(magacinForm);
+            obj.Invoke("GetAllItems");
+
+            ListBox listBoxItems = (ListBox)obj.GetFieldOrProperty("listBoxItems");
+
+            // Assert
+
+            Assert.AreEqual(3, listBoxItems.Items.Count);
+        }
+    }
+}
---
+++ MagacinPresentationLayerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MagacinPresentationLayerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MagacinPresentationLayerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("74ff04e3-76c6-4a71-96af-1c42b117ec4d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
